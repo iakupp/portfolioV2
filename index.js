7072655494298ec47614ec567900e17cfcef30c2
Jakub Majerčík <89073674+iakupp@users.noreply.github.com>
2024-06-27 22:49:13 +0200

    deploying form

--- index.js
+++ index.js
@@ -4,5 +4,29 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+const username = "impresse";
+const password = "jakubko33";
 
-app.bodyParser
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.get("/", (req,res) =>{
+    res.render("form.ejs",{});
+});
+
+app.post("/home", (req,res) => {
+    const userInputName = req.body.username;
+    const userInputPass = req.body.password;
+    if(userInputName == username && userInputPass == password){
+        res.render("main.ejs");
+    }
+    else{
+        res.status(404).send("Opps wrong input!")
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
